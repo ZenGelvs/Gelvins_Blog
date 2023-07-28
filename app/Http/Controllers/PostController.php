f78d4f090a23096879adc20fb02641b8b6d71c36
ZenGelvs <gelvin.gozar20@gmail.com>
2023-07-28 17:40:48 +0800

    Routes and Views

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,29 +4,48 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Requests\StorePostRequest;
 
 class PostController extends Controller
 {
-    public function createpost(Request $request){
-        $incomingFields = $request->validate([
-            'body' => 'required',
-            'title' => 'required'
-        ]);
+    //Create post and return to addpost view
+    public function store(StorePostRequest $request){
         
-        $incomingFields['title'] = strip_tags($incomingFields['title']);
-        $incomingFields['body'] = strip_tags($incomingFields['body']);
-
+        $incomingFields = $request->validated();
         Post::create($incomingFields);
-       return view ('/addpost');
+       return view ('posts');
     }
 
-
+    //show index view and contents of posts 
     public function index(){
         $posts = Post::all();
-        return view ('viewposts', ['posts' => $posts]);
+        return view ('view', ['posts' => $posts]);
     }
 
+    //return to addpost view
     public function return(){
-        return view ('/addpost');
+        return view ('posts');
     }
+
+    //edit function that retries data from db
+    public function edit(Post $posts){
+        return view('edit', ['posts'=>$posts]);
+    }
+
+    public function update(Post $posts, StorePostRequest $request){
+        $incomingFields = $request->validated();
+
+        $posts->update($incomingFields);
+
+        return view ('posts');
+    }
+
+    public function destroy(Post $posts){
+        $posts->delete();
+        return redirect('/');
+            
+    }
+
+    
+
 }
--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,25 @@ use App\Http\Controllers\PostController;
 */
 
 Route::get('/', function () {
-    return view('/addpost');
+    return view('/posts');});
+
+
+//Resource Controller
+Route::resource('posts', PostController::class);
+
+//Routegroup
+Route::group(['prefix'=> 'Posts.', 'as' => 'POSTS.'], function(){
+
+    Route::post('/create-post', [PostController::class, 'store']) ->name('store');
+
+    Route::post('/view-post', [PostController::class, 'index']) ->name('index');
+
+    Route::get('/return', [PostController::class, 'return']) ->name('return');
+
+    Route::get('edit-post/{posts}', [PostController::class, 'edit']) ->name('edit');
+
+    Route::put('edit-post/{posts}', [PostController::class, 'update']) ->name('update');
+
+    Route::delete('/delete-post/{posts}', [PostController::class, 'destroy']) ->name('destroy');
+
 });
-
-//Redirect to createpost view and Create an actual Post
-Route::post('/create-post',[PostController::class, 'createpost']);
-
-//Redirect to view posts
-Route::post('/view-post', [PostController::Class, 'index']);
-
-//From Index return to create post
-Route::get('/createpost-from-viewpost', [PostController::Class, 'return']);
-
