1a36ed2f3b9bdc78d0b360879ceab9f6e8ccda95
ZenGelvs <gelvin.gozar20@gmail.com>
2023-08-01 11:02:37 +0800

    Update edit.blade.php

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -3,17 +3,16 @@
 <div> 
     
     <h1 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white" > Edit Post </h1> <br><br>
-            <form action="{{route('posts.update', $post->id)}}" > 
+            <form action="{{route('posts.update', $post->id)}}" method="POST"> 
             @csrf
             @method('PUT')
             <input type="text" name="title" value="{{$post->Title}}"> <br><br>
             <textarea name="body">{{$post->Body}}</textarea> <br>
-            <a method="PUT" href="{{route('posts.update', $post->id)}}" class="mt-4 text-gray-500 dark:text-gray-400 c leading-relaxed">Save</a>
+            <button class="mt-4 text-gray-500 dark:text-gray-400 c leading-relaxed">Save</button>
             </form>
 
-            <form action="{{route('posts.index')}}" method='GET'> 
-                @csrf
+            
             <a href="{{route('posts.index')}}" class="mt-4 text-gray-500 dark:text-gray-400 c leading-relaxed"> Back </a>
-            </form>
+            
 </div> 
 @endsection
