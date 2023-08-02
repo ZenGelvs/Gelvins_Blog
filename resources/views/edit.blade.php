60455417a185b1adb6057aec816e1420b5c9eed4
ZenGelvs <gelvin.gozar20@gmail.com>
2023-08-02 09:06:11 +0800

    Ready for Merge

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,13 +1,13 @@
 @extends('layout.index')  
 @section('Content')
 <div> 
-    <h1 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white" > Edit Post </h1> <br><br>
+    <h1 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white"> Edit Post </h1> <br><br>
         <form action="{{route('post.update', $post->id)}}" method="POST"> 
             @csrf
             @method('PUT')
             <input type="text" name="title" value="{{$post->Title}}"> <br><br>
             <textarea rows="8" cols="70" name="body">{{$post->Body}}</textarea> <br>
-            <button class="mt-4 text-gray-500 dark:text-gray-400 c leading-relaxed">Save</button> <br>
+            <button class="mt-4 text-gray-500 dark:text-gray-400 c leading-relaxed"> Save </button> <br>
             <a href="{{route('post.index')}}" class="mt-4 text-gray-500 dark:text-gray-400 c leading-relaxed"> Back </a>
         </form>
 </div> 
--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -2,28 +2,28 @@
 @section('Content')
 <div> 
         <div>
-            <h1 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Create A new post</h1><br>
+            <h1 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white"> Create A new post </h1><br>
                 <form action="{{route('post.store')}}" method="POST">
                     @csrf
                     <input type="text" name="title" placeholder="Your Title"><br><br>
                     <textarea name="body" rows="8" cols="70" placeholder="Write your post here:"></textarea><br><br>
-                    <button class="mt-4 text-gray-500 dark:text-gray-400 c leading-relaxed"> Post </button>
+                    <button class="mt-4 text-gray-500 dark:text-gray-400 leading-relaxed"> Post </button>
                 </form>
         </div>
         <div  style= "padding:10px; margin:10px" > 
-            <h1 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white" > All posts  </h1>
+            <h1 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white"> All posts </h1>
             @foreach($post as $post)
             <div style="background-color: #1C3152; padding: 50px; margin-top: 80px; margin 20px 50px 20px 50px; padding: 40px 200px 60px 200px;"     
             class="dark:text-white"> 
-                <h3 style = "font-size: 60px;">{{$post['Title']}}</h3>
-                    <div style=" font-size: 30px;">
+                <h3 style="font-size: 60px;">{{$post['Title']}}</h3>
+                    <div style="font-size: 30px;">
                         {{$post['Body']}}
                     </div>
                 <a href="{{route('post.edit', $post->id)}}" class="mt-4 text-gray-500 dark:text-gray-400 c leading-relaxed" style="font-size:20px;">Edit</a>
                 <form action="{{route('post.destroy', $post->id)}}" method="POST"> 
                     @csrf
                     @method('DELETE')
-                    <button class="mt-4 text-gray-500 dark:text-gray-400 c leading-relaxed" style="font-size:20px;">Delete</button>
+                    <button class="mt-4 text-gray-500 dark:text-gray-400 c leading-relaxed" style="font-size:20px;"> Delete </button>
                 </form>
             </div>
             @endforeach
